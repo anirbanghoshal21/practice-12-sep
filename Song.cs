ff74c58ce285cba8acf11371aaffdde31460471f
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-09-13 10:48:12 +0530

    Part b prob 2

---
+++ Song.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CustomAttributes_practice
+{
+    [StereoDisc(Songname = "Faded" , Language = "English" , Lyrics = " So lost I'm faded") ]
+    public class Song
+    {
+        [StereoDisc(Songname = "Blackbird", Language = "English", Lyrics = "Take these broken wings")]
+        public string Name { get; set; }
+        [StereoDisc(Songname = "Thinking out loud", Language = "English", Lyrics = "Take me into your arms")]
+        public string Description { get; set; }
+
+        [StereoDisc(Songname = "How soon is now", Language = "English", Lyrics = "I am the son")]
+        public Song()
+        {
+
+        }
+       
+    }
+}
