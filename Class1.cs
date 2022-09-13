079ee979bf96d6a427d60f328763daa386ebbac8
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-09-13 10:00:37 +0530

    Part A Problem 1

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLibrary
+{
+    public class Calculation
+    {
+       
+        private double result;
+
+        public double Number
+        {
+            get;
+            set;
+        }
+       
+    }
+}
